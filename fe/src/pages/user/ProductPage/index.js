c40b4483151e25ad9081aca9f62c159f6e5bee8c
Minh1509 <nguyenquangminh15092003@gmail.com>
2024-08-05 15:15:00 +0700

    b10

---
+++ fe/src/pages/user/ProductPage/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "./style.scss";
+
+const ProductPage = () => {
+    return (
+        <div>
+            <h1> Danh sach san pham</h1>
+        </div>
+    )
+}
+export default ProductPage;
