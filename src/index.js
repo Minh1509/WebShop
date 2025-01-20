2912041e9cf2f0cae357a93a07406b879a8232d8
Minh1509 <nguyenquangminh15092003@gmail.com>
2025-01-20 10:09:20 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import RouterCustom from './router';
 import "./style/style.scss";
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
